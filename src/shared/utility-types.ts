952014ba0715e2d457c3abd512dc6f467c8556ca
Artem Sdobnikov <artem.sdobnikov25@gmail.com>
2022-04-29 17:58:21 +0300

    fix spelling mistake

--- src/shared/utility-types.ts
+++ src/shared/utility-types.ts
@@ -1,5 +1,5 @@
 /**
- * @description Constructs a type with all properties of `Type` set to optional and properties by `Keys` leave required
+ * @description Constructs a type with all properties of `Type` set to optional and properties by `Keys` leaves required
  */
 export type PartialExcept<Type, Keys extends keyof Type> = Partial<Type> &
   Pick<Type, Keys>;
