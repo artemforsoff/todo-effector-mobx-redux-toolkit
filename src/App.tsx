1ec4d10debc1a39e9a0e411aefe1812cd9195450
Artem Sdobnikov <artem.sdobnikov25@gmail.com>
2022-05-08 00:25:56 +0300

    add link

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import { TodoPage } from 'pages/todo';
 import { ExampleCounterPage, ExamplesOperatorsPage } from 'pages/examples';
 
 export const App = () => (
     <Routes>
-        <Route path="todo" element={<TodoPage />} />
-        <Route path="examples">
-            <Route path="operators" element={<ExamplesOperatorsPage />} />
-            <Route path="counter" element={<ExampleCounterPage />} />
+        <Route path="/">
+            <Route index element={<Link to="/todo">todo</Link>} />
+            <Route path="todo" element={<TodoPage />} />
+            <Route path="examples">
+                <Route path="operators" element={<ExamplesOperatorsPage />} />
+                <Route path="counter" element={<ExampleCounterPage />} />
+            </Route>
         </Route>
     </Routes>
 );
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-// import 'effector-logger/inspector';
+import 'effector-logger/inspector';
 import './index.scss';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
