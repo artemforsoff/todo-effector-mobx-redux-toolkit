8e2e11a28c220cedd4efe36a3b51b04f74d616b4
Artem Sdobnikov <artem.sdobnikov25@gmail.com>
2022-05-08 00:46:46 +0300

    redirect to index

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes, useNavigate } from 'react-router-dom';
 import { TodoPage } from 'pages/todo';
 import { ExampleCounterPage, ExamplesOperatorsPage } from 'pages/examples';
+import { useEffect } from 'react';
 
-export const App = () => (
-    <>
-        <Link to="/todo">todo</Link>
+export const App = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        navigate('/');
+    }, [navigate]);
+
+    return (
         <Routes>
-            <Route path="todo" element={<TodoPage />} />
-            <Route path="examples">
-                <Route path="operators" element={<ExamplesOperatorsPage />} />
-                <Route path="counter" element={<ExampleCounterPage />} />
+            <Route path="/">
+                <Route
+                    index
+                    element={
+                        <>
+                            <Link to="/todo">todo</Link>
+                            <Link to="/examples/operators">example: operators</Link>
+                            <Link to="/examples/counter">example: counter</Link>
+                        </>
+                    }
+                />
+                <Route path="todo" element={<TodoPage />} />
+                <Route path="examples">
+                    <Route path="operators" element={<ExamplesOperatorsPage />} />
+                    <Route path="counter" element={<ExampleCounterPage />} />
+                </Route>
             </Route>
         </Routes>
-    </>
-);
+    );
+};
