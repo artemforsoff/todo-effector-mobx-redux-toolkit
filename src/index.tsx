b6126c06e5ed14b58b01112a25ca8ea6fd9ef5c7
Artem Sdobnikov <artem.sdobnikov25@gmail.com>
2022-05-08 22:51:59 +0300

    remove gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import 'effector-logger/inspector';
 import './index.scss';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { App } from './App';
 import { store } from 'stores/redux-toolkit';
@@ -12,9 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <RecoilRoot>
         <Provider store={store}>
-            <HashRouter basename={process.env.REACT_APP_ROUTER_BASE_NAME}>
+            <BrowserRouter>
                 <App />
-            </HashRouter>
+            </BrowserRouter>
         </Provider>
     </RecoilRoot>
 );
